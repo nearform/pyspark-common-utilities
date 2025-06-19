3c02d15340b1fcaa5dea452271c4c15cc9f2955d
Hakim Pocketwalla <hakimpocketwalla@gmail.com>
2025-06-19 16:21:30 +0530

    added project files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyspark_utilities',
+    version='0.1.0',
+    description='Reusable PySpark utility functions for data pipelines',
+    author='Hakim Pocketwalla',
+    packages=find_packages(),
+    install_requires=[
+        'pyspark>=3.0.0',
+    ],
+    python_requires='>=3.8',
+)
