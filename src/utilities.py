9bed826d11eb726f2deb8e2bbeeaad485d8fbe42
Hakim Pocketwalla <hakimpocketwalla@gmail.com>
2025-06-20 17:27:31 +0530

    linter changes

--- src/utilities.py
+++ src/utilities.py
@@ -1,7 +1,7 @@
 """
 pyspark_utilities.py
 
-This module contains reusable PySpark utility functions commonly 
+This module contains reusable PySpark utility functions commonly
 used in data pipelines,
 such as removing duplicates and filling null values in DataFrames.
 """
@@ -13,13 +13,13 @@ def remove_duplicates(df, subset_cols=None):
     Removes duplicate rows from the DataFrame.
 
     :param df: Input DataFrame
-    :param subset_cols: List of columns to check for duplicates. 
+    :param subset_cols: List of columns to check for duplicates.
     If None, uses all columns.
     :return: Deduplicated DataFrame
     """
     return df.drop_duplicates(subset=subset_cols)
 
-# Further enhancement scope: add functionality to provide ordering columns and 
+# Further enhancement scope: add functionality to provide ordering columns and
 # keep first or last (use row number window function)
 
 
@@ -29,7 +29,7 @@ def fill_nulls(df, fill_dict):
     Fills null values based on a provided mapping.
 
     :param df: Input DataFrame
-    :param fill_map: Dictionary with column names as keys and fill 
+    :param fill_map: Dictionary with column names as keys and fill
     values as values.
     :return: DataFrame with nulls filled
     """
