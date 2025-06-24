573789ddd94f68f51b0ab3b6b2af6895f9ab2b78
Hakim Pocketwalla <hakimpocketwalla@gmail.com>
2025-06-24 15:55:08 +0530

    updated gitignore file and setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyspark_common_utilities",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "pyspark>=3.0.0",
@@ -13,7 +13,7 @@ setup(
     description="Reusable PySpark utility functions for data pipelines",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/nearform/pyspark_common_utilities",
+    url="https://github.com/nearform/pyspark-utilities",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
